4fe33495af65b383f909318783788e394cc313c8
Petros-Mystikos <lakmous2@gmail.com>
2023-12-04 22:11:44 +0200

    create userChoice and computerChoice
    create the two variable users and computers to compare them for the game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Rock-Papper-Scissors
+# Rock-Paper-Scissors
+
+I will create a simple version of rock-paper-scissors 
--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+let computerChoices = ['rock', 'paper', 'scissors'];
+let randomNumber = Math.floor(Math.random() * 3);
+
+let computerChoice = computerChoices[randomNumber];
+
+let userChoice = prompt('Choose your weapon against your opponent (rock, paper, scissors)');
+while( userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors'){
+    userChoice = prompt('Choose again, write the whole word  (rock, paper, scissors)');
+    userChoice = userChoice.toLowerCase();
+    console.log(userChoice);
+}
+console.log('done');
