c86570f765e1a581d3394e0a0aa602af9c2e66b8
Petros-Mystikos <lakmous2@gmail.com>
2023-12-05 09:35:50 +0200

    create the comparison between of PC and user
    create if statements to compare the outcome of the choices

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ let computerChoices = ['rock', 'paper', 'scissors'];
 let randomNumber = Math.floor(Math.random() * 3);
 
 let computerChoice = computerChoices[randomNumber];
+console.log(computerChoice);
 
 let userChoice = prompt('Choose your weapon against your opponent (rock, paper, scissors)');
 while( userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors'){
@@ -9,4 +10,39 @@ while( userChoice != 'rock' && userChoice != 'paper' && userChoice != 'scissors'
     userChoice = userChoice.toLowerCase();
     console.log(userChoice);
 }
-console.log('done');
+
+if (userChoice == 'rock'){
+    if (computerChoice == 'scissors'){
+        console.log( ' Your Opponent chose scissors. You win, rock beats scissors');
+    }
+    else if (computerChoice == 'paper'){
+        console.log('Your Opponent chose paper. You lose, rock loses to paper');
+    }
+    else{
+        console.log('Your Opponent chose rock. Its a tie, rock vs rock');
+    }
+}
+
+if (userChoice == 'paper'){
+    if (computerChoice == 'rock'){
+        console.log( ' Your Opponent chose rock. You win, paper beats rock');
+    }
+    else if (computerChoice == 'scissors'){
+        console.log('Your Opponent chose scissors. You lose, paper loses to scissors ');
+    }
+    else{
+        console.log('Your Opponent chose paper. Its a tie, paper vs paper');
+    }
+}
+
+if (userChoice == 'scissors'){
+    if (computerChoice == 'paper'){
+        console.log( ' Your Opponent chose paper. You win, scissors beats paper');
+    }
+    else if (computerChoice == 'rock'){
+        console.log('Your Opponent chose rock. You lose, scissors loses to rock ');
+    }
+    else{
+        console.log('Your Opponent chose scissors. Its a tie, scissors vs scissors');
+    }
+}
